a6e174ab31d54b8bd2f402b76ef6a9b9aedfa01b
Fantasy333 <1113082025@qq.com>
2017-12-22 10:08:42 +0800

    finish
    lab3

--- ensemble.py
+++ ensemble.py
@@ -1,53 +1,99 @@
 import pickle
+import numpy as np
+import math
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
-
+    
     def __init__(self, weak_classifier, n_weakers_limit):
         '''Initialize AdaBoostClassifier
-
         Args:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        
+        self.n_weakers_limit = n_weakers_limit
+        self.weak_classifier = weak_classifier
+        self.weak_classifiers = []
+        for i in range(n_weakers_limit):
+            self.weak_classifiers.append(weak_classifier(max_depth = 3))
+        
 
     def is_good_enough(self):
         '''Optional'''
         pass
 
+
     def fit(self,X,y):
         '''Build a boosted classifier from the training set (X, y).
-
-        Args:
+        Returns:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        print("newwwww")
+        w = 1.0 / X.shape[0] * np.ones((X.shape[0]))
+        self.alpha_array = np.empty((self.n_weakers_limit))
+        
+        for i in range(self.n_weakers_limit):
+            self.weak_classifiers[i].fit(X, y, sample_weight = w)
+            y_predict = np.sign(self.weak_classifiers[i].predict(X))
 
+            eps = 0
+            for j in range(y_predict.shape[0]):
+                if (y_predict[j] != y[j][0]):
+                    eps += w[j]
+            print(eps)
+            if (eps > 0.5):
+                self.n_weakers_limit = i
+                break
+                
+            self.alpha_array[i] = math.log((1 - eps) / eps) / 2
+            
+            Z = 0
+            exp_array = np.empty(w.shape)
+            for j in range(w.shape[0]):
+                e = math.exp(-1.0 * self.alpha_array[i] * y[j][0] * y_predict[j])
+                exp_array[j] = e
+                Z += w[j] * e
+                
+            for j in range(w.shape[0]):
+                w[j] = w[j] * exp_array[j] / Z
+        
 
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
-
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
-
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        predict_result = np.zeros((X.shape[0]))
+        
+        for i in range(self.n_weakers_limit):
+            y_predict = np.sign(self.weak_classifiers[i].predict(X))
+            predict_result += y_predict * self.alpha_array[i]
+        
+        return predict_result
+    
 
     def predict(self, X, threshold=0):
         '''Predict the catagories for geven samples.
-
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
             threshold: The demarcation number of deviding the samples into two parts.
-
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        predict_result = self.predict_scores(X)
+       
+        for i in range(X.shape[0]):
+            if (predict_result[i] >= threshold):
+                predict_result[i] = 1
+            elif (predict_result[i] < threshold):
+                predict_result[i] = -1
+            
+        return predict_result
+
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,52 @@
+from PIL import Image
+import numpy as np
 
+from sklearn.model_selection import train_test_split
+from ensemble import AdaBoostClassifier
+from feature import NPDFeature
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
 
 if __name__ == "__main__":
     # write your code here
-    pass
+    X=[]
+    y=[]
+    
+    for i in range(0,500):
+        path="C:\\Users\\Administrator\\Desktop\\ML2017-lab-03\\datasets\\original\\face\\face_%.3d.jpg" % (i)
+        img=Image.open(path).convert('L').resize((24,24))
+        X.append(NPDFeature(np.array(img)).extract())
+        y.append(1)
+        print(i)
 
+    for i in range(0,500):
+        path="C:\\Users\\Administrator\\Desktop\\ML2017-lab-03\\datasets\\original\\nonface\\nonface_%.3d.jpg" % (i)
+        img=Image.open(path).convert('L').resize((24,24))
+        X.append(NPDFeature(np.array(img)).extract())
+        y.append(-1)
+        print(i)
+        
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)  
+    ada = AdaBoostClassifier(DecisionTreeClassifier,20)
+
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    y_train = np.array(y_train)
+    y_test = np.array(y_test)
+    y_train = y_train.reshape((y_train.shape[0],1))
+    y_test = y_test.reshape((y_test.shape[0],1))
+    
+    ada.fit(X_train,y_train)
+    h = ada.predict(X_test)
+    yes=0
+    no=0
+    for i in range(0,len(h)):
+        if (h[i] == y_test[i]): yes += 1
+        if (h[i] != y_test[i]): no += 1
+    print(yes,"   ",no)
+    
+    report = classification_report(y_test, h, target_names=["nonface", "face"])
+    
+    file = open('report.txt', 'w')
+    file.write(report)
+    file.close()
